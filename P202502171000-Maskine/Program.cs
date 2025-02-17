c60a63b0b8880548449cb7a3eba75fb731062f75
Michell Cronberg~ <michell@cronberg.dk>
2025-02-17 12:10:47 +0100

    Terning mv

---
+++ P202502171000-Maskine/Program.cs
@@ -0,0 +1,50 @@
+﻿Maskine m = new Maskine();
+//m.LogDelegate = new LogDelegate(Console.WriteLine);
+m.LogDelegate = new Action<string>(Console.WriteLine);
+m.LogDelegate += Test;
+m.LogDelegate += Skriv;
+m.Start();
+m.Slut();
+
+
+void Test(string a)
+{
+    Console.WriteLine("******!!!! " + a);
+}
+
+void Skriv(string a)
+{
+    File.AppendAllText(@"c:\temp\data.log", a + "\r\n");
+}
+
+//delegate void LogDelegate(string t);
+
+class Maskine
+{
+
+    public Action<string> LogDelegate;
+
+    public void Start()
+    {
+        Log("Start");
+    }
+    public void Slut()
+    {
+        Log("Slut");
+    }
+
+    public Maskine()
+    {
+        this.LogDelegate = new Action<string>(Console.WriteLine);
+    }
+
+
+    private void Log(string txt)
+    {
+
+        string s = DateTime.Now.ToLongTimeString();
+        string l = $"{s} {txt}";
+        LogDelegate.Invoke(l);
+    }
+
+}
---
+++ P202502171100-Terning/Program.cs
@@ -0,0 +1,51 @@
+﻿
+//System.Timers.Timer t = new System.Timers.Timer(500);
+//t.Enabled = true;
+//t.Elapsed += Test;
+
+//Console.ReadLine();
+
+//System.IO.FileSystemWatcher a = new FileSystemWatcher(@"c:\temp");
+//a.EnableRaisingEvents = true;
+//a.Created += A_Created;
+//Console.ReadLine();
+//void A_Created(object sender, FileSystemEventArgs e)
+//{
+//    Console.WriteLine("* " + e.FullPath);
+//}
+
+
+
+
+
+
+Terning t = new Terning();
+
+
+
+public class Terning
+{
+
+    public Func<int, int, int> TilfældigDelegate;
+
+    public int Værdi { get; private set; }
+
+    public void Ryst()
+    {
+        if (TilfældigDelegate == null)
+        {
+            this.Værdi = 1;
+        }
+        else
+        {
+            this.Værdi = TilfældigDelegate(1, 7);
+        }
+
+    }
+
+
+    public Terning()
+    {
+        this.Ryst();
+    }
+}
