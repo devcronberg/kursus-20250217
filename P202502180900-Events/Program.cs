0aa7cb35cad4610a3037f54a4285d8f3258b80e8
Michell Cronberg~ <michell@cronberg.dk>
2025-02-18 09:24:43 +0100

    Add P202502180900-Events project with event handling
    - Introduced a new project "P202502180900-Events" in the solution.
    - Created project file targeting .NET 9.0 with implicit usings and nullable reference types.
    - Added `Maskine` class in `Program.cs` for event handling on start and stop actions.
    - Defined `StartEvent` and `StopEvent` with console logging.
    - Implemented `MitEventArg` class to hold event data for the stop event.

---
+++ P202502180900-Events/Program.cs
@@ -0,0 +1,37 @@
+﻿Maskine m = new Maskine();
+m.StartEvent += d => Console.WriteLine("Jeg starter - og er sidste startet " + d);
+m.StopEvent += (s,e) => Console.WriteLine("Jeg stopper - " + s.ToString() + " " + e.SidsteDato);
+
+m.Start();
+m.Stop();
+m.Start();
+
+
+class Maskine
+{
+    private DateTime sidsteStart;
+    public event Action<DateTime> StartEvent;
+    public event Action<object, MitEventArg> StopEvent;
+
+    public void Start() {
+        // log
+        StartEvent?.Invoke(sidsteStart);
+        sidsteStart = DateTime.Now;
+    }
+
+    public void Stop() {
+        // log
+        StopEvent?.Invoke(this, new MitEventArg() { SidsteDato = sidsteStart } );
+    }
+
+    public override string ToString()
+    {
+        return "Jeg er en maskine og er sidst startet kl. " + sidsteStart;
+    }
+
+}
+
+class MitEventArg : EventArgs {
+
+    public DateTime SidsteDato { get; set; }
+}
